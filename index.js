b591e090a29f317fecd3efa5b729d47f4c59ca7f
André Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2022-04-23 13:49:55 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+// Get the GitHub username input form
+const gitHubForm = document.getElementById('gitHubForm');
+
+// Listen for submissions on GitHub username input form
+gitHubForm.addEventListener('submit', (e) => {
+    
+    // Prevent default form submission action
+    e.preventDefault();
+
+    // Get the GitHub username input field on the DOM
+    let usernameInput = document.getElementById('usernameInput');
+
+    // Get the value of the GitHub username input field
+    let gitHubUsername = usernameInput.value;          
+
+    // Run GitHub API function, passing in the GitHub username
+    requestUserRepos(gitHubUsername);
+
+})
+
+
+function requestUserRepos(username){
+    
+    // Create new XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+    const url = `https://api.github.com/repos/${username}/Treasury-system-v2/contents/`;
+    // Replace -username- with your GitHub username, -repo- with the repository name, and then :path with a path to the file or folder you want to get the content of (leave blank to ge all files of the repository)
+
+    xhr.open('GET', URL, true);
+
+    xhr.onload = function() {
+        const data = JSON.parse(this.response);
+        
+        // Loop over each object in data array
+        for (let i in data) {
+
+            // Get the ul with id of of userRepos
+            let ul = document.getElementById('userRepos');
+    
+            // Create variable that will create li's to be added to ul
+            let li = document.createElement('li');
+            
+            // Add Bootstrap list item class to each li
+            li.classList.add('list-group-item')
+        
+            // Create the html markup for each li
+            li.innerHTML = (`
+                <p><strong>Repo:</strong> ${data[i].name}</p>
+                <p><strong>Description:</strong> ${data[i].description}</p>
+                <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+            `);
+            
+            // Append each li to the ul
+            ul.appendChild(li);
+        
+        }
+
+    }
+    
+    // Send the request to the server
+    xhr.send();
+    
+}
