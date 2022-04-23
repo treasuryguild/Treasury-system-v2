e746ad861c5b25a902ad821df112bfb2889201c0
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2022-04-23 14:05:44 +0200

    v2

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function requestUserRepos(username){
     
     // Create new XMLHttpRequest object
     const xhr = new XMLHttpRequest();
-    const url = `https://api.github.com/repos/${username}/Treasury-system-v2/contents/`;
+    const url = `https://api.github.com/repos/treasuryguild/Treasury-system-v2/contents/`;
     // Replace -username- with your GitHub username, -repo- with the repository name, and then :path with a path to the file or folder you want to get the content of (leave blank to ge all files of the repository)
 
     xhr.open('GET', URL, true);
