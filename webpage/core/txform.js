1f2d97947a48bc0f827ab84aedc5362109a7ea3a
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2022-04-24 16:47:31 +0200

    v2

--- webpage/core/txform.js
+++ webpage/core/txform.js
@@ -10,7 +10,10 @@ function getValue(name){
     const ada = getValue('ada')
     const description = getValue('description')
     const pool = getValue('pool')
+    const idea = getValue('ideaScale')
     const xrate = getValue('xrate')
+    const fund = getValue('fund')
+    const project = getValue('project')
     
     //generate a filename
     const filename = new Date().getTime().toString() + '-' + name.replace(/\s/g, '-') + ".json"
@@ -18,30 +21,16 @@ function getValue(name){
     
     //Generate a string mimicing the file structure
     //Indentation is important here
-    function ideascaleLink(pool) {
-      var iLink = "";
-      switch(pool) {
-        case 'Community Governance Oversight':
-          iLink = "https://cardano.ideascale.com/c/idea/383517";
-          break;
-        case 'Catalyst Audit Circle':
-          iLink = "https://cardano.ideascale.com/c/idea/381354";
-          break;
-        default:
-          iLink = "";
-          break;
-      }
-      return iLink;
-    }    
     
     //Generate a string mimicing the file structure
     //Indentation is important here
     let fileText = `{
   "id" : "${new Date().getTime().toString()}",
   "date": "${new Date().toUTCString()}",
-  "project": "Community Governance Oversight",
-  "pool": "${pool}",
-  "ideascale": "${ideascaleLink(pool)}",
+  "fund": "${fund}",
+  "project": "${project}",
+  "proposal": "${pool}",
+  "ideascale": "${idea}",
   "budget": "${budgetB}",
   "ada": "${ada}",
   "exchange-rate": "${xrate} USD per ADA",
@@ -57,58 +46,15 @@ function getValue(name){
     //Generate a github link with query parameter
     
     function githubQueryLink(pool) {
-      var answer = "";
-      switch(pool) {
-        case 'Community Governance Oversight':
-          answer = "Fund7/Community-Governance-Oversight/";
-          break;
-        case 'Catalyst Audit Circle':
-          answer = "Fund7/Catalyst-Audit-Circle/";
-          break;
-        default:
-          answer = "";
-          break;
-      }
+      var answer = fund + "/" + pool.replace(/\s/g, '-') + "/";
       return answer;
     }
 
     function githubQueryLink2(budgetB) {
-      var answer = "";
-      switch(budgetB) {
-        case 'Incoming IOG':
-          answer = "Incoming-IOG/";
-          break;
-        case 'Meetings':
-          answer = "Meetings/";
-          break;
-        case 'Comm/Org Tools':
-          answer = "Comm-Org-Tools/";
-          break;
-        case 'Surveys':
-          answer = "Surveys/";
-          break;
-        case 'Retrospectives':
-          answer = "Retrospectives/";
-          break;
-        case 'Reports':
-          answer = "Reports/";
-          break;
-        case 'Proposal':
-          answer = "Proposal/";
-          break;
-        case 'Town Hall Slides':
-          answer = "Town-Hall-Slides/";
-          break; 
-        case 'Meeting Minutes':
-          answer = "Meeting-Minutes/";
-          break;        
-        default:
-          answer = "";
-          break;
-      }
+      var answer = budgetB.replace(/\s/g, '-') + "/";
       return answer;
     }
     //Open in a new tab
-  window.open("https://github.com/treasuryguild/Treasury-system-v2/new/main/Transactions/" + githubQueryLink(pool) + githubQueryLink2(budgetB) + "new?value=" + encodedFileText +"&filename=" + filename);
+  window.open("https://github.com/treasuryguild/Treasury-system-v2/new/main/Transactions/" + project.replace(/\s/g, '-') + "/" + githubQueryLink(pool) + githubQueryLink2(budgetB) + "new?value=" + encodedFileText +"&filename=" + filename);
     
   }
